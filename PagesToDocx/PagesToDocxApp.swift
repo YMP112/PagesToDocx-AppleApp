ea9de4caf7540dc8b0201b4f2fdbb3a74ff48f8e
yaakov m. pines <y0583239121@gmail.com>
2025-05-27 19:15:27 +0300

    Initial Commit

---
+++ PagesToDocx/PagesToDocxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagesToDocxApp.swift
+//  PagesToDocx
+//
+//  Created by yaakov m. pines on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PagesToDocxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PagesToDocxTests/PagesToDocxTests.swift
@@ -0,0 +1,17 @@
+//
+//  PagesToDocxTests.swift
+//  PagesToDocxTests
+//
+//  Created by yaakov m. pines on 27/5/25.
+//
+
+import Testing
+@testable import PagesToDocx
+
+struct PagesToDocxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
